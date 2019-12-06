de85422a975d02c9c94b5f4e027d7771a49ff853
renault dzaky <dzakymon8@gmail.com>
2019-12-06 13:34:38 +0700

    rubah gambar

--- src/components/itemAnjing.vue
+++ src/components/itemAnjing.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="card" style="width: 30%;margin:20px">
-        <div class="images" style="height:300px;width:auto;background-image: url({{anjing.image_url}}); background-size:cover">
+        <div class="images" :style="`height:300px;width:auto;background-image: url('${anjing.image_url}'); background-size:cover`">
         </div>
         <div class="card-body" style="padding:40px;">
-          <h4 class="card-title">{{anjing.title}}</h4>
+          <h4 class="card-title">{{anjing.name}}</h4>
           <p class="card-text">{{anjing.description}}</p>
           <a href="#" class="btn btn-primary">details</a>
         </div>
